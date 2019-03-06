211460e42827044790a9ae12025caf6115d96c4a
Steve Astels <sastels@gmail.com>
2019-03-06 16:30:22 -0500

    add data and logic

--- src/App.js
+++ src/App.js
@@ -1,18 +1,34 @@
 import React, { Component } from 'react';
 import './App.css';
 
+const ministers = require("./ministers.json");
+const treasuryBoardStartDate = "2018-02-12"
+
+const daysSince = (dateString) => {
+  return Math.floor((new Date() - new Date(dateString)) / (1000*60*60*24))
+}
+
 class App extends Component {
+
+  constructor(props) {
+    super(props)
+    this.state = { ministers }
+  }
+
   render() {
-    const ministerCount = 2
-    const daysSinceChange = 4
-    const startDate = "2018-02-12"
+    const ministerCount = this.state.ministers.length
+    const currentMinister = this.state.ministers.slice(-1)[0]
+    const daysSinceChange = daysSince(currentMinister.startDate)
+
     return (
       <div className="App">
-      <h1>
-        I have had {ministerCount} Ministers since joining Treasury Board in {startDate}
-        </h1>
         <h1>
-          It has been {daysSinceChange} since getting a new Minister
+          I have had {ministerCount} Ministers since joining Treasury Board
+          on {treasuryBoardStartDate}.
+        </h1>
+        
+        <h1>
+          It has been {daysSinceChange} days since getting a new Minister.
         </h1>
       </div>
     );
